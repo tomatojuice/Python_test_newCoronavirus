4f2c8b0101b6b02590716d8e1c97ea83827e0f64
tomatojice <tomato.juice.7@gmail.com>
2020-02-25 23:07:15 +0900

    commit

--- test_of_python_newCoronavirus.py
+++ test_of_python_newCoronavirus.py
@@ -7,26 +7,26 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 # このパスはchromedriverをダウンロードしたパスを設定。
 # chromedriverとchromeのバージョンがあってないとエラーになる
-browser = webdriver.Chrome(executable_path='C:\\Users\\tomat\\chromedriver_win32\\chromedriver.exe')
-wait = WebDriverWait(browser, 10)
+browser = webdriver.Chrome(executable_path='C:\\Apps\\chromedriver_win32\\chromedriver.exe')
+wait = WebDriverWait(browser, 20)
 
-# 新浪のサイトから情報取得
 browser.get('https://news.sina.cn/zt_d/yiqing0121')
 
 provinces = []
 values = []
-for i in range(2,69,2):
-    province = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#mylist > div.mapCont.mapMoreCont > div.m_list > div:nth-child({}) > span.c1'.format(i))))
+for i in range(2, 69, 2):
+    province = wait.until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, '#mylist > div.mapCont.mapMoreCont > div.m_list > div:nth-child({}) > span.c1'.format(i))))
     provinces.append(province.text)
-    value = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#mylist > div.mapCont.mapMoreCont > div.m_list > div:nth-child({}) > span.c2'.format(i))))
+    value = wait.until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, '#mylist > div.mapCont.mapMoreCont > div.m_list > div:nth-child({}) > span.c2'.format(i))))
     values.append(value.text)
-
 
 def map_visualmap() -> Map:
     c = (
         Map()
-        .add("", [list(z) for z in zip(provinces, values)], "china")
-        .set_global_opts(
+            .add("", [list(z) for z in zip(provinces, values)], "china")
+            .set_global_opts(
             title_opts=opts.TitleOpts(title="中国のリアルタイムのコロナウイルス感染者数"),
             visualmap_opts=opts.VisualMapOpts(max_=600),
         )
@@ -34,4 +34,3 @@ def map_visualmap() -> Map:
     return c.render()
 
 map_visualmap()
-f
